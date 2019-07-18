d12d1ccad6f3e13e9956a59c160a41ae4ee80e0e
Said Washah <saidwashah@gmail.com>
2019-07-18 15:52:28 -0400

    finished filter

--- src/app/chat/grd-filter.pipe.ts
+++ src/app/chat/grd-filter.pipe.ts
@@ -5,11 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GrdFilterPipe implements PipeTransform {
   transform(items: any, filter: any): any {
-    if (!filter || filter==""){
+    console.log(items);
+    console.log(filter);
+    if (!filter || filter=="")
+    {
       return items;
-  }
-let obj = items.filter(chat=>chat.user2.firstName.includes(filter))
-console.log(obj.length)
-return obj;
+    }
+  
+    let obj = items.filter(chat=>chat.user2.firstName.toLowerCase().includes(filter))
+    
+    console.log(obj.length)
+      
+      return obj;
 }
 }
